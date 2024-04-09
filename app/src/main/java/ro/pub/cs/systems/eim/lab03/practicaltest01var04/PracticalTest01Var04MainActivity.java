4488d06590a461d1449cb1cb2a77a166561d73e9
ghiarad <dragos_stefan_13@yahoo.com>
2024-04-09 12:27:21 +0300

    B1

--- app/src/main/java/ro/pub/cs/systems/eim/lab03/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/lab03/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -1,6 +1,12 @@
 package ro.pub.cs.systems.eim.lab03.practicaltest01var04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,36 @@ public class PracticalTest01Var04MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button nextActivityButton = (Button)findViewById(R.id.nextButton);
+        CheckBox checkBox1 = (CheckBox)findViewById(R.id.checkBox1);
+        CheckBox checkBox2 = (CheckBox)findViewById(R.id.checkBox2);
+        EditText editText1 = (EditText)findViewById(R.id.editText1);
+        EditText editText2 = (EditText)findViewById(R.id.editText2);
+        Button displayButton = (Button)findViewById(R.id.displayButton);
+        TextView textView = (TextView)findViewById(R.id.textView);
+
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder text = new StringBuilder();
+                if (checkBox1.isChecked()) {
+                    if (editText1.getText().toString().isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "First edit text is empty", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    text.append(editText1.getText().toString());
+                }
+                if (checkBox2.isChecked()) {
+                    if (editText2.getText().toString().isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "Second edit text is empty", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    text.append(editText2.getText().toString());
+                }
+                textView.setText(text);
+            }
         });
     }
 }
